03f52240d3295f295cc46e9c44518dea0cc49d22
Realdo-Justino <realdojj@gmail.com>
2024-10-22 16:06:27 -0300

    Added Login and Password

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,50 @@
+import React, { useRef } from 'react';
 import './Login.css';
 
 function LoginPage() {
+    const inputLoginRef = useRef<HTMLInputElement|null>(null);
+    const inputPasswordRef = useRef<HTMLInputElement|null>(null);
+
+    const finishedEdittingLogin = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            inputPasswordRef.current?.focus();
+        }
+    }
+
+    const loginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(inputLoginRef.current?.value)
+        console.log(inputPasswordRef.current?.value)
+    }
+
     return (
-        <div className='MainPage'>
-            Login Page
+        <div className = 'MainPage'>
+            <div className = "LoginForm">
+                <div className = 'LoginTitle'>
+                    <header>
+                        <h1>Login</h1>
+                    </header>
+                </div>
+                <div className = 'LoginBody'>
+                    <form id = 'InputLogin' method = 'post' onSubmit = {loginSubmit}>
+                        <input
+                            ref = {inputLoginRef}
+                            id = 'inputUser'
+                            type = 'Text'
+                            placeholder = 'Input your Login'
+                            onKeyDown = {finishedEdittingLogin}
+                        />
+                        <input
+                            ref = {inputPasswordRef}
+                            id = 'inputPassword'
+                            type = 'password'
+                            placeholder = 'Input your Password'
+                        />
+                        <button>Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
