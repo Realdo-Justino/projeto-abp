be5eda7a35079294cf0b9c7c732719e1f0331b48
Realdo-Justino <realdojj@gmail.com>
2024-10-23 09:14:30 -0300

    Added Some Validation

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import Methods from '../../const/methods';
 import './Login.css';
 
 function LoginPage() {
@@ -14,8 +15,24 @@ function LoginPage() {
 
     const loginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(inputLoginRef.current?.value)
-        console.log(inputPasswordRef.current?.value)
+
+        let loginText : string|undefined = inputLoginRef.current?.value;            alert('Campo de login Vazio');
+        let passwordText : string|undefined = inputPasswordRef.current?.value;            alert('Campo de login Vazio');
+
+
+        if(Methods.isEmpthyText(loginText)) {
+            alert('Campo de login Vazio');
+            return;
+        }
+        if(Methods.isEmpthyText(passwordText)) {
+            alert('Campo de Senha Vazio');
+            return;
+        }
+
+
+        if((loginText  === 'test')||(passwordText === '1234')) {
+            console.log('Go to the next the next screen');
+        }
     }
 
     return (
