922f8aeafb312d734ae5ca35f857dc68327bdcf3
Rafael Cechinel Pacheco <rafaelcechinel4@gmail.com>
2024-11-06 15:19:57 -0300

    chat
    Envio de mensagens

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,7 +1,41 @@
-function ChatPage() {
-    return (
-        <div>Chat Room</div>
-    )
-}
+import { useState } from 'react'
 
-export default ChatPage;
+function Chat() {
+const []} =;
+
+function Chat() {
+const [mensagem, setMensagem] = useState('');
+const [mensagens, setMensagens] = useState([]);
+
+const adicionaItem = () => {
+    if (mensagem.trim()) {
+    setMensagens([...mensagens, mensagem]);
+      setMensagem('');}};
+
+const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    adicionaItem();};
+
+return (
+    <div className="chat-container">
+    <div className="lista">
+        {mensagens.map((msg, index) => (
+        <div key={index} className="mensagem">
+            <h3 className="voce">Você:</h3>
+            <p className="mensagens">{msg}</p>
+        </div>
+        ))}
+    </div>
+
+    <form id="formItem" onSubmit={handleSubmit}>
+        <input
+        className="Digitavel"
+        placeholder="Digite sua mensagem..."
+        value={mensagem}
+        onChange={(e) => setMensagem(e.target.value)}
+        />
+        <button className="botao" type="submit">Enviar</button>
+    </form>
+    </div>);}
+
+export default Chat;
