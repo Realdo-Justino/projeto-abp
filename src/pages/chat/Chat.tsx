13aef7083897a01d0f1016903080107a12a985c8
Rafael Cechinel Pacheco <rafaelcechinel4@gmail.com>
2024-11-06 15:39:55 -0300

    chat
    correção indexação.

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -1,41 +1,50 @@
 import { useState } from 'react'
 
 function Chat() {
-const []} =;
-
-function Chat() {
 const [mensagem, setMensagem] = useState('');
 const [mensagens, setMensagens] = useState([]);
 
-const adicionaItem = () => {
-    if (mensagem.trim()) {
-    setMensagens([...mensagens, mensagem]);
-      setMensagem('');}};
+const adicionaItem = () => 
+    {
+    if (mensagem.trim()) 
+        {
+            setMensagens([...mensagens, mensagem]);
+            setMensagem('');
+        }         
+    }
 
-const handleSubmit = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    adicionaItem();};
+const handleSubmit = 
+    (
+        event:{ 
+        preventDefault: () => void 
+        }) =>{
+                event.preventDefault();
+                adicionaItem()
+                }
 
 return (
     <div className="chat-container">
-    <div className="lista">
-        {mensagens.map((msg, index) => (
-        <div key={index} className="mensagem">
-            <h3 className="voce">Você:</h3>
-            <p className="mensagens">{msg}</p>
+        <div className="lista">
+            {
+                mensagens.map((msg, index) => (
+                    <div key={index} className="mensagem">
+                        <h3 className="voce">Você:</h3>
+                        <p className="mensagens">{msg}</p>
+                    </div>
+                ))
+            }
         </div>
-        ))}
-    </div>
 
-    <form id="formItem" onSubmit={handleSubmit}>
-        <input
-        className="Digitavel"
-        placeholder="Digite sua mensagem..."
-        value={mensagem}
-        onChange={(e) => setMensagem(e.target.value)}
-        />
-        <button className="botao" type="submit">Enviar</button>
-    </form>
-    </div>);}
+        <form id="formItem" onSubmit={handleSubmit}>
+            <input
+                className="Digitavel"
+                placeholder="Digite sua mensagem..."
+                value={mensagem}
+                onChange={(e) => setMensagem(e.target.value)}
+            />
+            <button className="botao" type="submit">Enviar</button>
+        </form>
+    </div>
+)}
 
 export default Chat;
